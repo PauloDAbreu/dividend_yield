aecb5412bf9541492123b8fe6b97fd375e8a519d
Paulo Abreu <paulovabreu97@gmail.com>
2025-04-06 20:57:30 -0300

    feat adicionado ultima data de atualização

--- dividend_yield.py
+++ dividend_yield.py
@@ -3,5 +3,7 @@ import yfinance as yf
 ticker = yf.Ticker("PETR4.SA")
 
 dy = ticker.info.get('dividendYield')
+ultimo_fechamento = ticker.info['regularMarketTime']
 
 print(f"dividend yield PETR4: {dy * 100:.2f}%")
+print(f"Última atualização: {ultimo_fechamento}")
